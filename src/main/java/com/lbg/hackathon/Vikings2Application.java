3a9e1d0996dd567b87eabda039de441822a834cb
Dinesh Rajineni <dineshrajineni21@gmail.com>
2024-07-25 20:26:54 +0530

    ok

--- src/main/java/com/lbg/hackathon/Vikings2Application.java
+++ src/main/java/com/lbg/hackathon/Vikings2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+
 @SpringBootApplication
 @EnableCaching
 public class Vikings2Application {
@@ -11,5 +12,6 @@ public class Vikings2Application {
     public static void main(String[] args) {
         SpringApplication.run(Vikings2Application.class, args);
     }
+
 
 }
--- src/main/java/com/lbg/hackathon/controller/RequestController.java
+++ src/main/java/com/lbg/hackathon/controller/RequestController.java
@@ -1,5 +1,10 @@
 package com.lbg.hackathon.controller;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
 import com.lbg.hackathon.entity.RequestDetails;
 import com.lbg.hackathon.entity.TeamDetails;
 import com.lbg.hackathon.exception.ResourceNotFoundException;
@@ -17,9 +22,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/requests")
@@ -89,7 +105,7 @@ public class RequestController {
     }
 
     @PatchMapping("/approve/{requestId}")
-    public ResponseEntity<RequestDetails> approveRequest(@PathVariable("requestId") Long requestId) throws ResourceNotFoundException, IOException {
+    public ResponseEntity<RequestDetails> approveRequest(@PathVariable("requestId") Long requestId) throws ResourceNotFoundException, IOException, ExecutionException, InterruptedException {
         RequestDetails requestDetails = requestRepository.findById(requestId).orElseThrow(() -> new ResourceNotFoundException("Request Not found"));
         RequestDetails approveRequest = requestDetails;
         if(!ObjectUtils.isEmpty(requestDetails)) {
@@ -100,7 +116,10 @@ public class RequestController {
                 .role(requestDetails1.getRequestorRole()).teamId(requestDetails1.getRequestorTeamId()).empId(requestDetails1.getRequestCreatedBy()).build();
 
        // publishMessage(postMessageDTO.toString());
+        String projectId = "lloyds-hack-grp-17";
+        String topicId = "ticketCreateUpdate";
 
+        publisherExample(projectId, topicId, postMessageDTO.toString());
 
         return new ResponseEntity<>(requestDetails1, HttpStatus.CREATED);
     }
@@ -122,4 +141,30 @@ public class RequestController {
 //            responseMessage = "Error publishing Pub/Sub message; see logs for more info.";
 //        }
 //    }
+
+    public void publisherExample(String projectId, String topicId, String message)
+            throws IOException, ExecutionException, InterruptedException {
+        TopicName topicName = TopicName.of(projectId, topicId);
+
+        Publisher publisher = null;
+        try {
+            // Create a publisher instance with default settings bound to the topic
+            publisher = Publisher.newBuilder(topicName).build();
+
+            ByteString data = ByteString.copyFromUtf8(message);
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+            // Once published, returns a server-assigned message id (unique within the topic)
+            ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+            String messageId = messageIdFuture.get();
+            System.out.println("Published message ID: " + messageId);
+        } finally {
+            if (publisher != null) {
+                // When finished with the publisher, shutdown to free up resources.
+                publisher.shutdown();
+                publisher.awaitTermination(1, TimeUnit.MINUTES);
+            }
+        }
+    }
+
 }
